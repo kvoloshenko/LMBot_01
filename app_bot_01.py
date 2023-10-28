c0daa0db57ba62728f8386bde26dcf95a4aa0bc1
kvoloshenko <kvoloshenko@gmail.com>
2023-10-28 17:47:07 +0300

    Refactoring

--- app_bot_01.py
+++ app_bot_01.py
@@ -3,33 +3,28 @@ import codecs
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
-# from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.embeddings import HuggingFaceEmbeddings
 import openai
 
 
 # Функция создания индексной базы знаний
 def create_index_db(database):
-  source_chunks = []
-  splitter = CharacterTextSplitter(separator="\n", chunk_size=1024, chunk_overlap=0)
+    source_chunks = []
+    splitter = CharacterTextSplitter(separator="\n", chunk_size=1024, chunk_overlap=0)
 
-  for chunk in splitter.split_text(database):
+    for chunk in splitter.split_text(database):
       source_chunks.append(Document(page_content=chunk, metadata={}))
 
-  # Initializing the embedding model
-  # embeddings = OpenAIEmbeddings()
-
-  model_id = 'sentence-transformers/all-MiniLM-L6-v2'
-  # model_kwargs = {'device': 'cpu'}
-  model_kwargs = {'device': 'cuda'}
-  embeddings = HuggingFaceEmbeddings(
+    model_id = 'sentence-transformers/all-MiniLM-L6-v2'
+    # model_kwargs = {'device': 'cpu'}
+    model_kwargs = {'device': 'cuda'}
+    embeddings = HuggingFaceEmbeddings(
       model_name=model_id,
       model_kwargs=model_kwargs
-  )
+    )
 
-  # Create an index db from separated text fragments
-  db = FAISS.from_documents(source_chunks, embeddings)
-  return db
+    db = FAISS.from_documents(source_chunks, embeddings)
+    return db
 
 def load_text(file_path):
     # Открытие файла для чтения
